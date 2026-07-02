7d2ebded143aa3f6884b7a9126ffd9a0d1521fa1
umang-parmar <umang2502parmar@gmail.com>
2026-07-02 11:33:05 +0530

    add get req & patiet.json sample data

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+@app.get("/")
+def hello():
+    return {'message':'Patient Management System API'}
+
+@app.get('/about')
+def about():
+    return {'message': 'A fully functional API to manage your patient records'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
+
