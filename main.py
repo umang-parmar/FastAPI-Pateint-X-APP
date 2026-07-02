bc30a99df1d9eee0246459d2cc43e65f8588a7f4
umang-parmar <umang2502parmar@gmail.com>
2026-07-02 22:58:05 +0530

    Patient Data Validation Pydantic Add

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 from fastapi import FastAPI , Path , HTTPException , Query
 import json
-
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal, Optional
 
 app = FastAPI()
 
+#pip install pydantic
+class Patient(BaseModel):
 
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')] # for option add import literal
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese'
+        
 def load_data():
     with open('patients.json', 'r') as f:
         data = json.load(f)
