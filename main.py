d83c988b86f3f76c9d16a08909cd356774d048e0
umang-parmar <umang2502parmar@gmail.com>
2026-07-03 06:39:14 +0530

    delete patient  req addded

--- main.py
+++ main.py
@@ -146,3 +146,18 @@ def update_patient(patient_id: str, patient_update: PatientUpdate):
     save_data(data)
 
     return JSONResponse(status_code=200, content={'message':'patient updated'})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+
+    # load data
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient deleted'})
