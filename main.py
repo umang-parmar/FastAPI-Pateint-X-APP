33fcc08e0cbdff7c8cd85a985bac2357ba17150a
umang-parmar <umang2502parmar@gmail.com>
2026-07-03 06:35:51 +0530

    update patient PUT req Add

--- main.py
+++ main.py
@@ -37,6 +37,14 @@ class Patient(BaseModel):
         else:
             return 'Obese'
         
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
+        
 def load_data():
     with open('patients.json', 'r') as f:
         data = json.load(f)
@@ -107,4 +115,34 @@ def create_patient(patient: Patient):
     # save into the json file
     save_data(data)
 
-    return JSONResponse(status_code=201, content={'message':'patient created successfully'})
+    return JSONResponse(status_code=201, content={'message':'patient created successfully'}) 
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    existing_patient_info = data[patient_id]
+    # patient_update is pydantic obj convert in dict 
+    updated_patient_info = patient_update.model_dump(exclude_unset=True) # exclude_unset:- if client send only city so only city show in dict not all field(Required)
+
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
+
+    # Reason: if weight change then bmi change thats why convert in pydantic object first one
+    #existing_patient_info -> pydantic object -> updated bmi + verdict
+    existing_patient_info['id'] = patient_id
+    patient_pydandic_obj = Patient(**existing_patient_info)
+    #-> pydantic object -> dict
+    existing_patient_info = patient_pydandic_obj.model_dump(exclude='id') # not required id so exclude it
+
+    # add this dict to data
+    data[patient_id] = existing_patient_info
+
+    # save data
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient updated'})
